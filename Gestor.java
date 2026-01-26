297e469bd8b6c50617e1787912ef9fcdf848a574
José Augusto da Rocha Gonçalves <101814599+joseaugustogoncalves@users.noreply.github.com>
2026-01-26 14:59:28 +0000

    Adicionei a classe Gestor ao projeto

---
+++ Gestor.java
@@ -0,0 +1,10 @@
+public class Gestor extends Funcionario {
+    private double bonus;
+
+    public Gestor(String nome, String cpf, double salarioBase, double bonus) {
+        super(nome, cpf, salarioBase);
+        this.bonus = bonus;
+    }
+
+    // Implementar o método calcularSalario
+}
