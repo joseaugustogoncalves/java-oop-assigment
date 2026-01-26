4f702c194cb87381707547e5192b4d455c6d1757
José Augusto da Rocha Gonçalves <101814599+joseaugustogoncalves@users.noreply.github.com>
2026-01-26 15:00:00 +0000

    Criação do arquivo Main.java com a classe principal.

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        // Demonstrar o funcionamento do sistema de funcionários
+        // Dica: Crie um Gestor e um Colaborador com salários diferentes
+    }
+}
